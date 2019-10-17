9f3842f518b8332e11ca99cd77a0ffe13c830755
Juliette Gerbaux <juliette.gerbaux@mines-paristech.fr>
2019-10-17 15:46:14 +0200

    Amorce non commentée

--- programme.py
+++ programme.py
@@ -1,1 +1,24 @@
+def find_seed(g,c=0,eps=2**(-26)):   
+    if g(0)<=c<=g(1):
+        a=0
+        b=1
+    elif g(1)<=c<=g(0):
+        a=1
+        b=0
+    else :
+        return None
+    while b-a>eps:
+        milieu=(a+b)/2
+        if c<=g(milieu):
+            b=milieu
+        else :
+            a=milieu
+    return (a+b)/2
+
+def f(x):
+    return 2*x-1
+
+find_seed(f)
+
+
 

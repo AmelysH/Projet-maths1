6acc98eb9612b8e4b5e38273cd57f3706e592527
Amélys Honvault <amelyshonvault@gmail.com>
2019-10-22 17:00:58 +0200

    première rédaction de contour

--- programme.py
+++ programme.py
@@ -1,12 +1,7 @@
 import autograd
 from autograd import numpy as np
-<<<<<<< HEAD
 import matplotlib
 from matplotlib import pyplot as plt
-=======
-
-
->>>>>>> db8f87144ff3210586605c357789f437d904e08a
 
 def find_seed(g,c=0,debut=0,fin=1,eps=2**(-26)):
     if g(debut)<=c<=g(fin):
@@ -40,7 +35,6 @@ def simple_contour(f,c=0.0,delta=0.01):
         return f(0,t)
 
     x=[0.0]
-<<<<<<< HEAD
     y=[find_seed(g,c)]
 
 
@@ -49,9 +43,6 @@ def simple_contour(f,c=0.0,delta=0.01):
         if x[-1]==None or y[-1]==None:
             return x[:-1],y[:-1]
         grad=grad_f(x[-1],y[-1])
-=======
-    
->>>>>>> db8f87144ff3210586605c357789f437d904e08a
         aux=[grad[1],-grad[0]]
         norme_aux=np.sqrt(aux[0]**2+aux[1]**2)
         vect_orthon=[delta/norme_aux*aux[0],delta/norme_aux*aux[1]]
@@ -60,12 +51,8 @@ def simple_contour(f,c=0.0,delta=0.01):
         if posx+delta>1 or posx-delta<0 or posy+delta>1 or posy-delta<0:
             posx=posx-2*vect_orthon[0]
             posy=posy-2*vect_orthon[1]
-<<<<<<< HEAD
             if posx+delta>1 or posx-delta<0 or posy+delta>1 or posy-delta<0:
                 return x,y
-=======
-                
->>>>>>> db8f87144ff3210586605c357789f437d904e08a
 
         if grad[0]==0.0:
             x.append(posx)
@@ -87,3 +74,51 @@ plt.close()
 x,y=simple_contour(f_test,0.5)
 plt.plot(x,y)
 plt.show()
+
+
+def contour(f,c=0.0,xc=[0.0,1.0], yc=[0.0,1.0], delta=0.01):
+    xs=[], ys=[]
+    for i in range(len(xc)-1):
+        debx=xc[i], finx=x[i+1]
+        deby=yc[i], finy=y[i+1]
+        ecartx=finx-debx
+        ecarty=finy-deby
+
+        def fad(x,y):
+            return f((x-debx)/ecartx,(y-deby)/ecarty)
+        xad1,yad1 = simple_contour(fad,c,delta)
+        x1=(np.array(xa1)*ecartx+debx).tolist()
+        y1=(np.array(ya1)*ecarty+deby).tolist()
+        xs.append(x1)
+        ys.append(y1)
+
+
+        def gad(x,y):
+            return f((y-deby)/ecarty,(x-debx)/ecartx)
+        xad2,yad2 = simple_contour(gad,c,delta)
+        x2=(np.array(xa2)*ecartx+debx).tolist()
+        y2=(np.array(ya2)*ecarty+deby).tolist()
+        xs.append(x2)
+        ys.append(y2)
+
+        def had(x,y):
+            return f(1-((x-debx)/ecartx),(y-deby)/ecarty)
+        xad3,yad3 = simple_contour(had,c,delta)
+        x3=(np.array(xa3)*ecartx+debx).tolist()
+        y3=(np.array(ya3)*ecarty+deby).tolist()
+        xs.append(x3)
+        ys.append(y3)
+
+        def iad(x,y):
+            return f((x-debx)/ecartx,1-((y-deby)/ecarty))
+        xad4,yad4 = simple_contour(iad,c,delta)
+        x4=(np.array(xa4)*ecartx+debx).tolist()
+        y4=(np.array(ya4)*ecarty+deby).tolist()
+        xs.append(x4)
+        ys.append(y4)
+
+    return xs,ys
+
+
+        
+        
